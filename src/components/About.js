9d120ffd84eba601351e8a6d9fe445445a6f8403
xavierthorbjornsen <xavierthorbjornsen@gmail.com>
2022-03-21 11:09:25 +0930

    more mobile responsive changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,71 +1,97 @@
 import { Card, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import React from "react";
-import { 
+import {
     DiGit,
     DiReact,
-    DiPython, 
-    DiDotnet, 
-    DiFirebase, 
+    DiPython,
+    DiDotnet,
+    DiFirebase,
     DiRasberryPi,
     DiGithubBadge,
-    DiVisualstudio,} from 'react-icons/di';
+    DiVisualstudio,
+} from 'react-icons/di';
 import { SiSolidity, SiArduino, SiVisualstudiocode, SiMysql, SiCsharp } from 'react-icons/si';
 import { useMediaQuery } from 'react-responsive';
 
 
 
 const useStyles = makeStyles((theme) => ({
-    sectionRoot: {
+    sectionDesktop: {
         opacity: 0.9,
         height: '90%',
     },
+    sectionMobile: {
+        opacity: 0.9,
+        height: '90%',
+        width: '90vw',
+    }
 }))
 
 
 
 export default function About() {
     const classes = useStyles();
-    const isMonitor = useMediaQuery({ query: '(min-width: 1433px)' })
-    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1432px)' })
+    const isMonitor = useMediaQuery({ query: '(min-width: 1042px)' })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1043px)' })
 
 
     return (
-        <div className={classes.sectionRoot}>
-            <Card elevation={3} sx={{ width: 270, p: 2, mt:2, display: "flex-grow", flexDirection: 'column' }}>
-                <Typography variant="body1" >Hi, my name is Xavier and I enjoy open-source developement.
-                    I have been developing in these languages for a while now.</Typography>
-                    {isMonitor && <ul>
+        <div>
+            <div className={classes.sectiondesktop}>
+                {isMonitor && <div> <Card elevation={3} sx={{ width: "40vw", p: 2, mt: 2, display: "flex-grow", flexDirection: 'column' }}>
+                    <Typography variant="body1" >Hi, my name is Xavier and I enjoy open-source developement.
+                        I have been developing in these languages for a while now.</Typography>
+                    <ul>
                         <li ><DiPython size={30} color={"black"} /> Python</li>
                         <li ><DiDotnet size={30} color={"Blue"} /> Dotnet</li>
                         <li ><DiReact size={30} color={"#40e0d0"} /> React</li>
-                        <li ><SiCsharp size={30}  /> C Sharp</li>
+                        <li ><SiCsharp size={30} /> C Sharp</li>
                         <li ><SiSolidity size={30} /> Solidity</li>
-                    </ul>}
-                    {isTabletOrMobile && <div ><DiPython size={30} color={"black"} /><DiDotnet size={30} color={"Blue"} /><DiReact size={30} color={"#40e0d0"} /><SiCsharp size={30} /><SiSolidity size={30} />
-                    </div>}
-            </Card>
-            <Card elevation={3} sx={{ height: 'auto',  width: 270, p: 2, mt:2, display: "flex-grow", }}>
-                <Typography variant="body1" >I have used these plaforms for various projects.</Typography>
-                        {isMonitor && <ul>
-                        <li ><SiVisualstudiocode size={26} color={"#2196f3"} /> Visual Studio Code</li>
-                        <li ><DiGit size={30} /> Git</li>
-                        <li ><DiGithubBadge size={30} /> Github</li>
-                        <li ><DiRasberryPi size={30} color={"Darkred"}/> RaspberryPi</li>
-                        <li ><DiFirebase size={30} color={"#ff9800"}/> FireBase </li>
-                        <li ><SiArduino size={30} color={"#40e0d0"}/>Arduino </li>
-                        <li><DiVisualstudio size={30} color={"#AB345A"} /> Visual Studio</li>
-                        <li><SiMysql size={30} /> My SQL</li>
-                    </ul>}
-                    {isTabletOrMobile && <div><SiVisualstudiocode size={28} color={"#2196f3"} /> 
+                    </ul>
+                </Card>
+
+                    <Card elevation={3} sx={{ height: 'auto', width: "40vw", p: 2, mt: 2, display: "flex-grow", }}>
+                        <Typography variant="body1" >I have used these plaforms for various projects.</Typography>
+                        <ul>
+                            <li ><SiVisualstudiocode size={26} color={"#2196f3"} /> Visual Studio Code</li>
+                            <li ><DiGit size={30} /> Git</li>
+                            <li ><DiGithubBadge size={30} /> Github</li>
+                            <li ><DiRasberryPi size={30} color={"Darkred"} /> RaspberryPi</li>
+                            <li ><DiFirebase size={30} color={"#ff9800"} /> FireBase </li>
+                            <li ><SiArduino size={30} color={"#40e0d0"} />Arduino </li>
+                            <li><DiVisualstudio size={30} color={"#AB345A"} /> Visual Studio</li>
+                            <li><SiMysql size={30} /> My SQL</li>
+                        </ul>
+                    </Card>
+
+                </div>
+                }
+            </div>
+            <div className={classes.sectionMobile}>
+            {isTabletOrMobile && <div><Card elevation={3} sx={{  p: 2, mt: 2, }}>
+                <Typography variant="body1" >Hi, my name is Xavier and I enjoy open-source developement.
+                        I have been developing in these languages for a while now.</Typography>
+                        <SiVisualstudiocode size={28} color={"#2196f3"} /> 
                     <DiGit size={30} />
                     <DiGithubBadge size={30} />
                     <DiRasberryPi size={30} color={"Darkred"}/>
                     <DiFirebase size={30} color={"#ff9800"}/>
                     <SiArduino size={30} color={"#40e0d0"}/>
                     <DiVisualstudio size={30} color={"#AB345A"} />
-                    <SiMysql size={30} /></div>}
-            </Card>
+                    <SiMysql size={30} />
+                </Card>
+                <Card elevation={3} sx={{  p: 2, mt: 2, }}>
+                        <Typography variant="body1" >I have used these plaforms for various projects.</Typography>
+                        <DiPython size={30} color={"black"} />
+                        <DiDotnet size={30} color={"Blue"} />
+                        <DiReact size={30} color={"#40e0d0"} />
+                        <SiCsharp size={30} />
+                        <SiSolidity size={30} />
+                    </Card>
+                </div>
+            }
+            </div>
         </div>
     )
 };
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -12,9 +12,13 @@ import { useMediaQuery } from 'react-responsive';
 const useStyles = makeStyles((theme) => ({
     sectionExperience: {
         opacity: 0.8,
-        maxWidth: 270,
+        width: "40vw",
         marginBottom: 20,
-        
+    },
+    sectionExperienceMobile: {
+        opacity: 0.8,
+        marginBottom: 20,
+        width: '90vw'
     }
 }))
 
@@ -25,39 +29,46 @@ const Experience = () => {
     const isMonitor = useMediaQuery({ query: '(min-width: 1042px)' })
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1043px)' })
     return (
-        <div className={classes.sectionExperience}>
-            {isMonitor && <Stack spacing={2}>
-                <Card sx={{ height:200, }}>
-                    <Typography variant="h4" sx={{ paddingLeft: 2,  }}>ARRQ <GiDeliveryDrone color="#001f17" /></Typography>
-                    <Typography variant="h5"  sx={{ paddingLeft: 2, }}>Chief Technical Officer </Typography>
-                    <Typography variant="body1" sx={{paddingLeft: 2, }}>Build, design and write python scripts to automate the flight of drones. Example code below.<br /> <Link underline="none" href={url} >Python flight <HiOutlineExternalLink /></Link> </Typography>
-                </Card>
-                <Card sx={{ height:200, }}>
-                    <Typography variant="h4" sx={{ paddingLeft: 2, }}>NEC </Typography>
-                    <Typography variant="h5"  sx={{paddingLeft: 2, }}>Systems Engineer <br /></Typography>
-                    <Typography variant="body1" sx={{paddingLeft: 2,  }}>Build applications and updating existing repositories using DotNet/SQL/WPF, and react.</Typography>
-                </Card>
-                <Card sx={{ height:200, }}>
-                    <Typography variant="h4" sx={{ paddingLeft: 2,  }}>Territory Generation</Typography>
-                    <Typography sx={{paddingLeft: 2 }}>IT Support</Typography>
-                </Card>
-            </Stack>}
-            {isTabletOrMobile && <Stack spacing={2}>
-                <Card sx={{ height:150, }}>
-                    <Typography variant="h5" sx={{ paddingLeft: 2,  }}>ARRQ <GiDeliveryDrone color="#001f17" /></Typography>
-                    <Typography variant="h6"  sx={{ paddingLeft: 2, }}>Chief Technical Officer </Typography>
-                    <Typography variant="body1" sx={{paddingLeft: 2, }}>Build, design and write python scripts to automate the flight of drones. Example code below.</Typography>
-                </Card>
-                <Card sx={{ height:150, }}>
-                    <Typography variant="h5" sx={{ paddingLeft: 2, }}>NEC </Typography>
-                    <Typography variant="h6"  sx={{paddingLeft: 2, }}>Systems Engineer <br /></Typography>
-                    <Typography variant="body1" sx={{paddingLeft: 2,  }}>Build applications and updating existing repositories using .Net/SQL/WPF, and React.js</Typography>
-                </Card>
-                <Card sx={{ height:150, }}>
-                    <Typography variant="h5" sx={{ paddingLeft: 2,  }}>Territory Generation</Typography>
-                    <Typography variant="h6" sx={{paddingLeft: 2 }}>IT Support</Typography>
-                </Card>
-            </Stack>}
+        <div>
+            <div className={classes.sectionExperience}>
+                {isMonitor && <Stack spacing={2}>
+                    <Card sx={{ height: 200, }}>
+                        <Typography variant="h4" sx={{ paddingLeft: 2, }}>ARRQ <GiDeliveryDrone color="#001f17" /></Typography>
+                        <Typography variant="h5" sx={{ paddingLeft: 2, }}>Chief Technical Officer </Typography>
+                        <Typography variant="body1" sx={{ paddingLeft: 2, }}>Build, design and write python scripts to automate the flight of drones. Example code below.<br /> <Link underline="none" href={url} >Python flight <HiOutlineExternalLink /></Link> </Typography>
+                    </Card>
+                    <Card sx={{ height: 200, }}>
+                        <Typography variant="h4" sx={{ paddingLeft: 2, }}>NEC </Typography>
+                        <Typography variant="h5" sx={{ paddingLeft: 2, }}>Systems Engineer <br /></Typography>
+                        <Typography variant="body1" sx={{ paddingLeft: 2, }}>Build applications and updating existing repositories using DotNet/SQL/WPF, and react.</Typography>
+                    </Card>
+                    <Card sx={{ height: 200, }}>
+                        <Typography variant="h4" sx={{ paddingLeft: 2, }}>Territory Generation</Typography>
+                        <Typography sx={{ paddingLeft: 2 }}>IT Support</Typography>
+                        <Typography variant="body1" sx={{ paddingLeft: 2, }}>Helped manage servers and network configurations</Typography>
+                    </Card>
+                </Stack>}
+
+            </div>
+            <div className={classes.sectionExperienceMobile}>
+                {isTabletOrMobile && <Stack spacing={2}>
+                    <Card sx={{ height: 150, }}>
+                        <Typography variant="h5" sx={{ paddingLeft: 2, }}>ARRQ <GiDeliveryDrone color="#001f17" /></Typography>
+                        <Typography variant="h6" sx={{ paddingLeft: 2, }}>Chief Technical Officer </Typography>
+                        <Typography variant="body1" sx={{ paddingLeft: 2, }}>Build, design and write python scripts to automate the flight of drones. Example code below.</Typography>
+                    </Card>
+                    <Card sx={{ height: 150, }}>
+                        <Typography variant="h5" sx={{ paddingLeft: 2, }}>NEC </Typography>
+                        <Typography variant="h6" sx={{ paddingLeft: 2, }}>Systems Engineer <br /></Typography>
+                        <Typography variant="body1" sx={{ paddingLeft: 2, }}>Build applications and updating existing repositories using .Net/SQL/WPF, and React.js</Typography>
+                    </Card>
+                    <Card sx={{ height: 150, }}>
+                        <Typography variant="h5" sx={{ paddingLeft: 2, }}>Territory Generation</Typography>
+                        <Typography variant="h6" sx={{ paddingLeft: 2 }}>IT Support</Typography>
+                        <Typography variant="body1" sx={{ paddingLeft: 2, }}>Helped manage servers and network configurations</Typography>
+                    </Card>
+                </Stack>}
+            </div>
         </div>
 
     )
